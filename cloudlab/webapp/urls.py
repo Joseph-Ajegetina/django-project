d030130a3ff73971752e1445d902f6f68ccc02ba
Joseph-Ajegetina <josephajongbah@gmail.com>
2025-09-18 12:34:36 +0000

    reorganize and make more orderly

--- cloudlab/webapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('tasks/', views.task_list, name='task_list'),
-    path('add/', views.add_task, name='add_task'),
-]
